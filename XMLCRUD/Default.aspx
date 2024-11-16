d0fbcb3f98f7391645c54aba223cc29efcfcbc33
Joy Munshi <70768803+Joy201002143@users.noreply.github.com>
2024-11-16 12:49:36 +0600

    Add project files.

---
+++ XMLCRUD/Default.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="XMLCRUD._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <center>
+
+        <asp:Label ID="Label2" runat="server" Text="CRUD using Employee XML"></asp:Label>
+        <br />
+        <br />
+        <br />
+
+    <asp:TextBox ID="txtID" runat="server" Placeholder="ID"></asp:TextBox>
+        <br />
+    <br />
+    <asp:TextBox ID="txtName" runat="server" Placeholder="Name"></asp:TextBox>
+        <br />
+    <br />
+    <asp:TextBox ID="txtDepartment" runat="server" Placeholder="Department"></asp:TextBox>
+    <br />
+    <br />
+    <br />
+    <br />
+    <asp:Button ID="btnAdd" runat="server" OnClick="btnAdd_Click" Text="Add" />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="btnUpdate" runat="server" OnClick="btnUpdate_Click" Text="Update" Width="105px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+    <asp:Button ID="btnDelete" runat="server" OnClick="btnDelete_Click" Text="Delete" />
+    <br />
+        <br />
+        <br />
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="View All" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="You can also select by DropDown"></asp:Label>
+        <br />
+    <br />
+        <asp:DropDownList ID="DropDownList1" AutoPostBack="true" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+        </asp:DropDownList>
+    <br />
+    <br />
+    <asp:TextBox ID="txtSearch" runat="server" Placeholder="Search by Name"></asp:TextBox>
+&nbsp;&nbsp;
+    <asp:Button ID="btnSearch" runat="server" OnClick="btnSearch_Click" Text="Search" />
+    <br />
+    <br />
+    <asp:GridView ID="gvEmployees" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+        <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+        <EditRowStyle BackColor="#999999" />
+        <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+        <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+        <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+        <SortedAscendingCellStyle BackColor="#E9E7E2" />
+        <SortedAscendingHeaderStyle BackColor="#506C8C" />
+        <SortedDescendingCellStyle BackColor="#FFFDF8" />
+        <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+    </asp:GridView>
+        </center>
+</asp:Content>
---
+++ XMLCRUD/Default.aspx.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Web.UI;
+using System.Data;
+using System.Xml;
+using System.Web.UI.WebControls;
+
+namespace XMLCRUD
+{
+    
+
+    public partial class _Default : Page
+    {
+        private string xmlFilePath;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            xmlFilePath = Server.MapPath("~/App_Data/data.xml");
+            if (!IsPostBack)
+            {
+                LoadEmployees();
+                BindDropdown();
+
+            }
+
+        }
+
+        private void BindDropdown()
+        {
+            DataSet ds = new DataSet();
+            ds.ReadXml(xmlFilePath);
+
+            if (ds.Tables["Employee"] != null)
+            {
+                DropDownList1.DataSource = ds.Tables["Employee"];
+                DropDownList1.DataTextField = "Name";  
+                DropDownList1.DataValueField = "ID";  
+                DropDownList1.DataBind();
+
+                DropDownList1.Items.Insert(0, new ListItem("-- Select Employee --", "0"));
+            }
+        }
+
+        private void LoadEmployees()
+        {
+            DataSet ds = new DataSet();
+            ds.ReadXml(xmlFilePath);
+            gvEmployees.DataSource = ds.Tables["Employee"];
+            gvEmployees.DataBind();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xmlFilePath);
+
+            XmlElement newEmployee = doc.CreateElement("Employee");
+
+            XmlElement id = doc.CreateElement("ID");
+            id.InnerText = txtID.Text;
+            newEmployee.AppendChild(id);
+
+            XmlElement name = doc.CreateElement("Name");
+            name.InnerText = txtName.Text;
+            newEmployee.AppendChild(name);
+
+            XmlElement department = doc.CreateElement("Department");
+            department.InnerText = txtDepartment.Text;
+            newEmployee.AppendChild(department);
+
+            doc.DocumentElement.AppendChild(newEmployee);
+            doc.Save(xmlFilePath);
+
+            LoadEmployees();
+            ClearFields();
+
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xmlFilePath);
+
+            XmlNode employee = doc.SelectSingleNode($"/Employees/Employee[ID='{txtID.Text}']");
+            if (employee != null)
+            {
+                employee["Name"].InnerText = txtName.Text;
+                employee["Department"].InnerText = txtDepartment.Text;
+
+                doc.Save(xmlFilePath);
+                LoadEmployees();
+                ClearFields();
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xmlFilePath);
+
+            XmlNode employee = doc.SelectSingleNode($"/Employees/Employee[ID='{txtID.Text}']");
+            if (employee != null)
+            {
+                doc.DocumentElement.RemoveChild(employee);
+                doc.Save(xmlFilePath);
+
+                LoadEmployees();
+                ClearFields();
+
+            }
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            ds.ReadXml(xmlFilePath);
+
+            DataTable employeeTable = ds.Tables["Employee"];
+            if (employeeTable != null)
+            {
+                DataView dv = new DataView(employeeTable);
+                dv.RowFilter = $"Department LIKE '%{txtSearch.Text}%' OR ID LIKE '%{txtSearch.Text}%' OR Name LIKE '%{txtSearch.Text}%'";
+                gvEmployees.DataSource = dv;
+                gvEmployees.DataBind();
+                ClearFields();
+            }
+        }
+        private void ClearFields()
+        {
+            txtID.Text = string.Empty;
+            txtName.Text = string.Empty;
+            txtDepartment.Text = string.Empty;
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            LoadEmployees();
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            ds.ReadXml(xmlFilePath);
+
+            DataTable employeeTable = ds.Tables["Employee"];
+            if (employeeTable != null)
+            {
+
+                if (DropDownList1.SelectedValue == "0" ) {
+                    LoadEmployees();
+                }
+                else
+                {
+                    DataView dv = new DataView(employeeTable);
+                    dv.RowFilter = $"Department LIKE '%{DropDownList1.SelectedValue}%' OR ID LIKE '%{DropDownList1.SelectedValue}%' OR Name LIKE '%{DropDownList1.SelectedValue}%'";
+                    gvEmployees.DataSource = dv;
+                    gvEmployees.DataBind();
+                }
+                ClearFields();
+            }
+
+        }
+    }
+}
